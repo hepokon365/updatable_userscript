fe66bf8d6e0a4b18947799bdc193dac8eed899c5
hepokon365 <hepokon365@gmail.com>
2020-06-07 23:45:42 +0900

    add sample script.

---
+++ src/sample.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Updatable Userscript
+// @namespace    https://github.com/hepokon365/updatable_userscript
+// @version      1.0.0
+// @description  updatable userscript sample.
+// @author       hepokon365
+// @match        https://example.com/
+// @grant        none
+// @updateURL    https://github.com/hepokon365/updatable_userscript/raw/master/src/sample.user.js
+// @downloadURL  https://github.com/hepokon365/updatable_userscript/raw/master/src/sample.user.js
+// @supportURL   https://github.com/hepokon365/updatable_userscript
+// ==/UserScript==
+
+(() => {
+  console.log('v1.0.0');
+})();
